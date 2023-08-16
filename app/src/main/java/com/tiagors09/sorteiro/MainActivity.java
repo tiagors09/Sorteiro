3f899daab0fd20ad3636834a6e4528ec388a5dad
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-08-15 21:45:39 -0300

    Adicionado código de seleção de número pseudo-aleatório e mudança na view

--- app/src/main/java/com/tiagors09/sorteiro/MainActivity.java
+++ app/src/main/java/com/tiagors09/sorteiro/MainActivity.java
@@ -3,6 +3,8 @@ package com.tiagors09.sorteiro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void raffleNumber(View v) {
+        TextView drawnNumber = findViewById(R.id.drawnNumber);
+        drawnNumber.setText(Double.toString(Math.random() * 10));
     }
 }
